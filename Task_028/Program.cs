c58607824a54ec9e5b3c8d77d1edb2c5783b67da
loki47 <larsentuta49@gmail.com>
2023-06-26 19:38:21 +0300

    Task_028/

---
+++ Task_028/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int N = Prompt("Введите число: ");
+int result = ProductNum(N);
+Console.WriteLine($"{N} -> {result}");
+
+int ProductNum(int N)// описание метода нахождения произведения
+{
+    int product = 1;
+    for (int i = 1; i <= N; i++)// пока i меньше N , N умножаем на i+1
+    {
+        product *= i;
+    }
+    return product;
+}
+
+int Prompt(string message)//описание метода (введите число)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
