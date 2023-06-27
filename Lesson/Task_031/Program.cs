790b36cb406bd321251c95281deb0912ee112eaa
loki47 <larsentuta49@gmail.com>
2023-06-27 16:49:13 +0300

    Сумма положительных и отрицательных элементов в массиве

--- Lesson/Task_031/Program.cs
+++ Lesson/Task_031/Program.cs
@@ -10,15 +10,16 @@ int minValue = Prompt("Введите минимальное число диап
 int maxValue = Prompt("Введите максимальное число диапазона в массиве: ");
 int[] array = GetArray(size, minValue, maxValue);
 PrintArray(array);
+GetSumm(array);
 
-int Prompt(string message)
+int Prompt(string message)// работа с пользователем
 {
     Console.WriteLine(message);
     int number = int.Parse(Console.ReadLine());
     return number;
 }
 
-int[] GetArray(int size, int minValue, int maxValue)
+int[] GetArray(int size, int minValue, int maxValue)// заполнение массива
 {
     int[] result = new int[size];
     for (int i = 0; i < result.Length; i++)
@@ -28,7 +29,7 @@ int[] GetArray(int size, int minValue, int maxValue)
     return result;
 }
 
-void PrintArray(int[] arr)
+void PrintArray(int[] arr)// печать массива
 {
     // for (int i = 0; i < arr.Length; i++)
     // {
@@ -40,14 +41,16 @@ void PrintArray(int[] arr)
     }
 }
 
-
-int positiveSum = 0;
-int negativeSum = 0;
-
-foreach (int el in array)// цикл будет проходить по всем элементам массива
+void GetSumm(int[] array)// находим сумму положительных и отрицательных элементов
 {
-    if (el > 0) positiveSum += el;
-    if (el < 0) negativeSum += el;
-}
+    int positiveSum = 0;
+    int negativeSum = 0;
 
-Console.WriteLine($"Сумма положительных чисел = {positiveSum}, сумма отрицатльных чисел = {negativeSum}");
+    foreach (int el in array)// цикл будет проходить по всем элементам массива
+    {
+        if (el > 0) positiveSum += el;
+        if (el < 0) negativeSum += el;
+    }
+
+    Console.WriteLine($"Сумма положительных чисел = {positiveSum}, сумма отрицатльных чисел = {negativeSum}");
+}
