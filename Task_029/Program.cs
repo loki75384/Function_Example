129f0a74363c183320d3ce2376ae4ae4f4667b7e
loki47 <larsentuta49@gmail.com>
2023-06-27 14:32:16 +0300

    Заполнение и печать массива

---
+++ Task_029/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int Prompt(string message)//описание метода (введите число)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
+
+int lenght = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение для случайного диапазона: ");
+int max = Prompt("Конечнон значение для случайного диапазона: ");
+int[] array = GenerateArray(lenght, min, max);
+PrintArray(array);
