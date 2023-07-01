45b69595a02c4f915bbb4d33dd07b24bbc9b9745
loki47 <larsentuta49@gmail.com>
2023-07-01 14:31:12 +0300

    Задача про стороны треугольника

---
+++ Lesson/Task_040/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 40: Напишите программу, которая принимает на
+// вход три числа и проверяет, может ли существовать
+// треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+int a = Prompt("Введите число 1: ");
+int b = Prompt("Введите число 2: ");
+int c = Prompt("Введите число 3: ");
+
+if (TriangleInequality(a, b, c))
+    Console.WriteLine($"Треугольник со сторонами такой длины может существовать");
+else
+    Console.WriteLine($"Треугольник со сторонами такой длины не может существовать");
+
+bool TriangleInequality(int a, int b, int c)
+{
+    bool inequality = true;
+
+    if (a >= (b + c)) inequality = false;
+    if (b >= (a + c)) inequality = false;
+    if (c >= (b + a)) inequality = false;
+
+    return inequality;
+
+}
+
+
+int Prompt(string message)// работа с пользователем
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
