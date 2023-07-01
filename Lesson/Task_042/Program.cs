3795e8ca3d346e2c52fcac23c6921ee9d4261b8a
loki47 <larsentuta49@gmail.com>
2023-07-01 18:20:37 +0300

    Конвертация в двоичную систему

---
+++ Lesson/Task_042/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 42: Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10 
+
+// string val = (Console.ReadLine()!);
+// int from = 10;
+// int to = 2;
+
+// string binary = Convert.ToString(Convert.ToInt32(val, from), to);// ToString(Int32, Int32)	
+// //Преобразует значение заданного 32-битового целого числа со знаком в эквивалентное строковое представление в указанной системе счисления.
+// Console.WriteLine(binary);
+
+int num = Prompt("Введите число: ");
+Console.WriteLine(BinaryConvert(num));
+
+int Prompt(string message)// работа с пользователем
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string BinaryConvert(int num)
+{
+    string newValue = "";
+    while (num >= 1)
+    {
+        newValue = num % 2 + newValue;
+        num /= 2;
+    }
+    return newValue;
+}
