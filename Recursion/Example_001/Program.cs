a04945cba3df8de65d1a67c1f2bf8cabb5501dfa
loki47 <larsentuta49@gmail.com>
2023-07-04 17:25:44 +0300

    собрать строку с числами от a до b, a <= b

---
+++ Recursion/Example_001/Program.cs
@@ -0,0 +1,9 @@
+﻿// собрать строку с числами от a до b, a <= b;
+
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+    else return string.Empty;
+}
+Console.WriteLine(NumbersRec(1, 10));
