99e61046d2f4f8971a944822c088ee2fc5b6e276
loki47 <larsentuta49@gmail.com>
2023-07-06 17:11:55 +0300

    Рекурсия

---
+++ Recursion/Example_003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Факториал числа
+
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialRec(n -1);
+}
+Console.WriteLine(FactorialRec(10)); // 3628800
+
+// Возвести a в степень n
+
+int PowerRec(int a, int n)
+{
+    if (n == 0) return 1;
+    else return PowerRec(a, n -1) * a;
+}
+Console.WriteLine(PowerRec(2,10));// 1024
+
+// Перебор слов.
