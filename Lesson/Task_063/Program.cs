a77bcfaa36d9923dfba8bcf64f3109726008bb98
loki47 <larsentuta49@gmail.com>
2023-07-10 15:11:53 +0300

    Рекурсия. Вывод чисел от 1 до N

---
+++ Lesson/Task_063/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int N = Convert.ToInt32(Console.ReadLine());// ввод числа N
+Console.WriteLine($"{Range(N)}");
+
+string Range(int n)
+{
+    if (n == 1)
+        return "1";
+    return Range(n-1) + ", " + Convert.ToInt32(n);
+}
