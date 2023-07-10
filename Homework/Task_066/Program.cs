73837df03927965b126b42d89d228df08bfeee11
loki47 <larsentuta49@gmail.com>
2023-07-10 16:55:41 +0300

    Task_066

---
+++ Homework/Task_066/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = Prompt("Введите число M: ");
+int N = Prompt("Введите число N: ");
+SumFromMToN(M, N);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    m++;
+    res = m + SumMN(m, n);
+    return res;
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
