87138e8e97f6546f77e9ed043c27cfdb3cd81c8f
loki47 <larsentuta49@gmail.com>
2023-07-10 15:59:28 +0300

    Рекурсия. Сумма цифр числа

---
+++ Lesson/Task_067/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int num = Prompt("Введите число: ");
+Console.WriteLine($"{GetSumDigit(num)}");
+
+int Prompt(string message)// работа с пользователем
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int GetSumDigit(int n)
+{
+    if (n == 0)
+        return 0;
+    return GetSumDigit(n / 10) + n % 10;    
+}
+
